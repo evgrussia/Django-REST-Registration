c17563ca56edc81b2c920ff3ca5a62878a669c9d
Evgussia <evgrussia@gmail.com>
2022-12-19 14:47:42 +0000

    change default port

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sharedlinks.settings")
+    runserver.default_port = "8088"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
